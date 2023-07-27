23a52a4351b7181448ed526363e678e7ca96ee44
Ali Aljufairi <ali.redha.ali@gmail.com>
2023-07-27 12:21:13 +0300

    fix threading

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import argparse
 import epics
 import json
 import os
-import threading
 from PIL import Image
+import threading
+
 class StepScan:
-    def __init__(self, exposure_time, overall_distance, step_size, detector_pv, motion_stage_pv,camera_acq_pv, image_size_x, image_size_y, image_counter, num_images, acq_mode, start_acq, acq_status, trigger_mode, trigger_source, trigger_software, image_data):
+    def __init__(self, exposure_time, overall_distance, step_size, detector_pv, motion_stage_pv, camera_acq_pv, image_size_x, image_size_y, image_counter, num_images, acq_mode, start_acq, acq_status, trigger_mode, trigger_source, trigger_software, image_data):
         self.exposure_time = exposure_time
         self.overall_distance = overall_distance
         self.step_size = step_size
@@ -26,16 +27,24 @@ class StepScan:
         self.trigger_software = trigger_software
         self.image_data = image_data
 
-
     def move_motor_to_position(self, position):
         self.motion_stage.move(position)
-        while not self.motion_stage.done_moving:  # Wait until the motion is done
+        while not self.motion_stage.done_moving:
             time.sleep(0.1)
+
+    def save_image(self, image_data, file_name, image_size_x, image_size_y):
+        if not os.path.exists("images"):
+            os.makedirs("images")
+
+        image_reshaped = np.reshape(image_data, (image_size_y, image_size_x))
+        file_path = os.path.join("images", file_name.replace("npy", "png"))
+        image_pil = Image.fromarray(image_reshaped)
+        image_pil.save(file_path)
 
     def acquire_image(self, camera_acq_pv, acq_status, image_data):
         # Function to acquire image asynchronously
 
-        # Set acquisition mode to continuous
+        # Set acquisition mode to multiple
         epics.caput(self.acq_mode, 1)
         # Set trigger mode to "On"
         epics.caput(self.trigger_mode, 1)
@@ -44,50 +53,48 @@ class StepScan:
         # Set trigger software to "1" (trigger)
         epics.caput(self.trigger_software, 1)
 
-        def image_acquisition_thread():
-            # Thread function to acquire the image
-            epics.caput(camera_acq_pv, 1)
-            while epics.caget(acq_status) == 1:
-                print("Acquiring image...")
-                time.sleep(0.1)
+        def check_acquisition_started():
+            # Thread function to check if image acquisition has started
+            initial_image_counter = epics.caget(self.image_counter)
+            time.sleep(0.5)  # Wait for a short time to allow image acquisition to start
+            current_image_counter = epics.caget(self.image_counter)
 
-            # Retrieve the image data
-            image_data = epics.caget('FLIR5:image1:ArrayData')
+            if current_image_counter != initial_image_counter:
+                print("Image acquisition has started.")
+            else:
+                print("Image acquisition has not started yet.")
 
-            # Save the acquired image with a file name based on the timestamp (if needed)
-            # file_name = f"image_{time.time()}.png"
-            # self.save_image(image_data, file_name)
+        # Create and start the thread to check image acquisition
+        check_acquisition_thread = threading.Thread(target=check_acquisition_started)
+        check_acquisition_thread.start()
 
-        # Create and start the image acquisition thread
-        image_thread = threading.Thread(target=image_acquisition_thread)
-        image_thread.start()
+        def acquisition_callback(value, **kwargs):
+            # Callback function to monitor the acquisition status
+            if value == 0:
+                print("Image acquisition is complete.")
 
-        # Check whether the image counter changed or not (for demonstration purposes)
-        initial_image_counter = epics.caget(self.image_counter)
-        time.sleep(0.5)  # Wait for a short time to allow image acquisition to start
-        current_image_counter = epics.caget(self.image_counter)
+        # Register the acquisition callback
+        callback_id = epics.camonitor(acq_status, callback=acquisition_callback, run=True)
 
-        if current_image_counter != initial_image_counter:
-            print("Image acquisition has started.")
-        else:
-            print("Image acquisition has not started yet.")
+        # Start the image acquisition using software trigger
+        epics.caput(camera_acq_pv, 1)
 
+        # Optionally, you can add a delay here to wait for the acquisition to complete if needed
+        # time.sleep(10)  # Wait for 10 seconds (adjust as needed)
 
+        # Unregister the callback when image acquisition is complete
+        epics.camonitor_clear(callback_id)
 
-    def save_image(self, image_data, file_name,image_size_x,image_size_y):
-        # Create the "images" directory if it does not exist
-        if not os.path.exists("images"):
-            os.makedirs("images")
+        # Retrieve the image data
+        image_data = epics.caget(image_data)
 
-        # Reshape image according to predefined size
-        image_size_x = epics.caget(image_size_x)
-        image_size_y = epics.caget(image_size_y) 
-        image_reshaped = np.reshape(image_data, (image_size_y, image_size_x))
+        # Save the acquired image with a file name based on the timestamp (if needed)
+        # timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+        # file_name = f"image_{timestamp}.png"
+        # self.save_image(image_data, file_name, self.image_size_x, self.image_size_y)
 
-        # Save the image in the "images" folder as PNG
-        file_path = os.path.join("images", file_name.replace("npy", "png"))
-        image_pil = Image.fromarray(image_reshaped)
-        image_pil.save(file_path)
+        # Return the acquired image data (if needed)
+        return image_data
 
     def start_step_scan(self):
         num_steps = int(self.overall_distance / self.step_size)
@@ -97,16 +104,14 @@ class StepScan:
                 target_position = step * self.step_size
                 self.move_motor_to_position(target_position)
                 print(f"target pos:        {target_position}")
-                image_data = self.acquire_image(self.camera_acq_pv,self.acq_status,self.image_data)
+                image_data = self.acquire_image(self.camera_acq_pv, self.acq_status, self.image_data)
                 timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 data_file.write(f"{target_position}    {self.motion_stage.get('RBV')}    {timestamp}\n")
                 # Save the acquired image with a file name based on the timestamp
                 # file_name = f"image_{timestamp}.npy"
-                # self.save_image(image_data, file_name)
-
+                # self.save_image(image_data, file_name, self.image_size_x, self.image_size_y)
 
 def main(args):
-    # Read PV names from the JSON file
     with open(args.config_file) as json_file:
         config = json.load(json_file)
         detector_pv = config.get("detector_pv")
@@ -142,8 +147,6 @@ def main(args):
         trigger_source,
         trigger_software,
         image_data
-
-
     )
     step_scan.move_motor_to_position(0)  # Move to the home position (position 0)
     step_scan.start_step_scan()
