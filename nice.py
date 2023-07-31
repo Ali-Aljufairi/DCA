47002de691ba62022c37b09760ad4f0984c81e85
Ali Aljufairi <ali.redha.ali@gmail.com>
2023-07-31 09:31:30 +0300

    araay extract

---
+++ nice.py
@@ -0,0 +1,32 @@
+import numpy as np
+from PIL import Image
+import os
+
+x = 1000
+y = 1000
+
+if not os.path.exists('images'):
+    os.makedirs('images')
+
+with open('data.txt') as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines):
+    if line.strip() == 'Detector #1 data:':
+        data = lines[i+1].split()
+        data = [int(d) for d in data]
+
+        # Debugging checks
+        print(f"Data size: {len(data)}")
+        print(f"Data array: {data}")
+
+        data = np.array(data)
+
+        # Verify if data size matches expected shape
+        if data.size != x * y:
+            raise ValueError(f"Data size ({data.size}) does not match the expected shape ({x}x{y})")
+
+        data = data.reshape(x, y)
+
+        img = Image.fromarray(data)
+        img.save(f'images/image{i}.png')
