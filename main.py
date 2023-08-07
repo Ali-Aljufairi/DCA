bbc5ec22a598356a3eaaa1eaf0d7e6fbd6dd6a6b
Ali Aljufairi <ali.redha.ali@gmail.com>
2023-08-07 14:39:16 +0300

    fix class structure 3

--- main.py
+++ main.py
@@ -48,28 +48,6 @@ class ContinuousScan:
         self.zmq_port = 1234
         self.zmq_host = "localhost"
 
-    def setup_hdf5_file(self):
-        # Create or open an HDF5 file to store the data
-        with h5py.File(self.hdf_file, "w") as f:
-            # Create the root group
-            root_group = f.create_group("scan_data")
-
-            # Add metadata to the root group
-            root_group.attrs["exposure_time"] = self.exposure_time
-            root_group.attrs["total_distance"] = self.total_distance
-            root_group.attrs["step_size"] = self.step_size
-            root_group.attrs["num_images"] = self.num_images
-            root_group.attrs["acq_mode"] = self.acq_mode
-            root_group.attrs["trigger_mode"] = self.trigger_mode
-            root_group.attrs["trigger_source"] = self.trigger_source
-
-            # Create the image data group
-            data_group = root_group.create_group("image_data")
-
-            # Add image size metadata
-            data_group.attrs["image_size_x"] = self.image_size_x
-            data_group.attrs["image_size_y"] = self.image_size_y
-
     def calculate_total_time(self, fps):
         time_per_frame = 1/fps
         self.total_time = time_per_frame * self.total_distance
@@ -95,19 +73,9 @@ class ContinuousScan:
         return float(self.constant_distance)
 
     def move_epics_motor(self, position):
-        # Move the motor to the desired position
         self.motion_stage.move(position)
         while not self.motion_stage.done_moving:
             time.sleep(0.1)
-
-        print(f"Motor moved to position: {position}")
-
-    def save_image_to_hdf5(self, data_group, step, target_position, image_data):
-        dataset_name = f"image_{step}"
-        data_group.create_dataset(dataset_name, data=image_data)
-        img_dataset = data_group[dataset_name]
-        img_dataset.attrs["distance"] = target_position
-        img_dataset.attrs["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
 
     def setup_camera(self):
         epics.caput(self.acq_mode, 1)
@@ -120,86 +88,23 @@ class ContinuousScan:
         self.setup_camera()
         self.motion_stage = epics.Motor(self.motion_stage_pv)
         fps = epics.caget(self.fps_pv)
-
-        # Calculate the required parameters
+        self.move_epics_motor(300)
         self.calculate_velocity(fps)
         accel_d = self.calculate_accel_distance()
         print(f"accel_d: {accel_d}, type: {type(accel_d)}")
-
-        print(f"Moving to position 0 - accel_d...")
         self.move_epics_motor(0 - float(accel_d))
-        print("Starting the scan...")
         print(f"Accelerating to steady speed...")
         self.move_epics_motor(self.total_distance + float(accel_d))
 
-        # Steady speed
-        print("Acquiring data at steady speed...")
 
-        # write function to get the data from the ZMQ socket
-        #
-        #
-        #
-        def get_data_from_zmq_socket():
-            context = zmq.Context()
-            socket = context.socket(zmq.PULL)
-            socket.connect(f"tcp://127.0.01:1234")
-            received_data = socket.recv()
-            return received_data
-        received_data = get_data_from_zmq_socket
-        print(f"Received data: {received_data}")
 
         data_group = self.setup_hdf5_file()
 
-        # Reshape and save the received data
-        image_data = np.array(received_data, dtype=np.uint8).reshape(
-            self.image_size_y, self.image_size_x)
 
-        self.save_image_to_hdf5(
-            data_group, 0, self.motion_stage.position, image_data)
 
-        print("Scan completed.")
-        print(f"Saved data in {self.hdf_file}")
 
-    def reshape_and_save_image_chunk(self, data_chunk, chunk_idx, stepscan_obj):
-        data = np.frombuffer(data_chunk, dtype=np.uint8)
-        data = data.reshape(
-            (stepscan_obj.image_size_y, stepscan_obj.image_size_x))
-        return data
 
-    def reshape_and_save_worker(self, client_id, data_chunk, chunk_idx, stepscan_obj, data_group):
-        reshaped_data = self.reshape_and_save_image_chunk(
-            data_chunk, chunk_idx, stepscan_obj)
-        with data_group.get_lock():
-            stepscan_obj.save_image_to_hdf5(
-                data_group, chunk_idx, stepscan_obj.motion_stage.position, reshaped_data)
-        print(
-            f"Client {client_id} reshaped and saved data for chunk {chunk_idx}")
 
-    def client_worker(self, client_id, data_chunks, stepscan_obj, data_group, sync_event):
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://localhost:1234")
-
-        sync_event.wait()  # Wait for the synchronization event from the server
-        sync_event.clear()  # Clear the event for future synchronization
-
-        data = socket.recv()
-
-        # write data to txt file for debugging
-        with open(f"client_{client_id}.txt", "wb") as f:
-            f.write(data)
-
-        chunk_size = stepscan_obj.image_size_x * stepscan_obj.image_size_y
-        num_chunks = 2448*2048 // chunk_size
-
-        for chunk_idx in range(num_chunks):
-            chunk_start = chunk_idx * chunk_size
-            chunk_end = chunk_start + chunk_size
-            data_chunk = data[chunk_start:chunk_end]
-            self.reshape_and_save_worker(
-                client_id, data_chunk, chunk_idx, stepscan_obj, data_group)
-
-    # function to get the zmq data from the server after image acquisition to be passe to start_processes
 
     def start_processes(self, zmq_port, zmq_host):
         # Create a Queue to hold the data chunks
@@ -219,46 +124,7 @@ class ContinuousScan:
         zmq_process.join()
         reshape_process.join()
 
-    def receive_zmq_data(self, zmq_port, zmq_host, chunk_queue):
-        context = zmq.Context()
-        socket = context.socket(zmq.PULL)
-        socket.connect(f"tcp://{zmq_host}:{zmq_port}")
 
-        while True:
-            data_chunk = socket.recv()
-            if data_chunk == b"EOF":  # Assuming EOF indicates end of data
-                break
-            chunk_queue.put(data_chunk)
-
-    def receive_data(self, data, chunk_queue):
-        chunk_size = self.image_size_x * self.image_size_y
-        num_chunks = len(data) // chunk_size
-
-        for i in range(num_chunks):
-            start = i * chunk_size
-            end = start + chunk_size
-            chunk = data[start:end]
-            chunk_queue.put(chunk)
-
-    def reshape_and_save(self, chunk_queue, stepscan_obj):
-        chunk_idx = 0
-        while True:
-            chunk = chunk_queue.get()
-            if chunk is None:
-                break
-
-            data = np.frombuffer(chunk, dtype=np.uint8)
-            image_data = data.reshape(
-                (stepscan_obj.image_size_y, stepscan_obj.image_size_x))
-
-            with stepscan_obj.data_group.get_lock():
-                stepscan_obj.save_image_to_hdf5(
-                    stepscan_obj.data_group, chunk_idx, stepscan_obj.motion_stage.position, image_data)
-
-            print(f"Reshaped and saved chunk {chunk_idx}")
-            chunk_idx += 1
-
-        chunk_queue.put(None)  # Signal complete
 
 
 def main(args):
